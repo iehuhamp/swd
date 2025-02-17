cc6a8f34325d8468df1d13d2f4fd11aa24f0691d
iehuhamp <hieuphamtrung2910@gmail.com>
2025-02-17 23:45:03 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MovieManagement from "./pages/movie-management";
+import MoviesManagement from "./pages/movie-management";
+
+function App() {
+  return <MoviesManagement />;
+}
+
+export default App;
